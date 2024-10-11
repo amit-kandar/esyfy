337912870587ec428fa75c960f329171bd6106e5
Amit Kandar <kandaramit2001@gmail.com>
2024-10-11 18:28:46 +0530

    feat: add few assets

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,5 @@
 export const LOGO = "ESYFY"
+export const HERO_ILLUSTRATION_MOBILE = require('../assets/Hero_Illustration_asset_mobile.webp');
+export const HERO_ILLUSTRATION_DESKTOP = require('../assets/home-bg.webp');
+export const HERO__ILLUSTRATION_WIDGET_1 = require('../assets/home-widget-1.png');
+export const HERO__ILLUSTRATION_WIDGET_2 = require('../assets/home-widget-2.png');
